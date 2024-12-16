b1d530c9bbbdc0a69fe52d606acd5fd2ed365dfd
Hanumant-Shinde-777 <hanumantshinde1213@gmail.com>
2024-12-16 13:47:41 +0530

    div created for cards and cards

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -9,7 +9,35 @@ const Main = () => {
     <img src={assets.user_icon} alt="" />
   
 </div>
+<div className="main_container">
+  <div className="greet">
+    <p><span>Hello jack</span></p>
+    <p>How can I help you today?</p>
+  </div>
+  <div className="cards">
+    <div className="card">
+      <p>Suggest beautiful place to see on an upcoming road trip</p>
+      <img src={assets.compass_icon} alt="" />
+    </div>
+    {/* Another dev  */}
+    <div className="card">
+      <p>Briefly describe your problem</p>
+      <img src={assets.bulb_icon} alt="" />
+    </div>
 
+    <div className="card">
+      <p>Brainstom team bonding activities for our work restreat</p>
+      <img src={assets.message_icon} alt="" />
+    </div>
+
+    <div className="card">
+      <p>Improve the readability of the following code</p>
+      <img src={assets.code_icon} alt="" />
+    </div>
+
+    
+  </div>
+</div>
     </div>
   )
 }
