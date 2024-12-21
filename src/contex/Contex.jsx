cfb0d59c76b61db17729fbb309c5e9b7232c9e29
Hanumant-Shinde-777 <hanumantshinde1213@gmail.com>
2024-12-21 14:10:36 +0530

    build a delay para function

--- src/contex/Contex.jsx
+++ src/contex/Contex.jsx
@@ -11,15 +11,37 @@ const ContextProvider = (props) => {
     const[loading, setLoading] = useState(false);
     const[resultData, setResultData] = useState("");
 
+    const delayPara =(index, nextword) =>{
+      setTimeout(function(){
+        setResultData((prev) => prev + nextword);
+      },75*index)
+    }
+
 
     const onSent = async (prompt) => {
       setResultData("");
       setLoading(true);
       setShowResult(true);
       setRecentPrompt(input);
+      setPrevPrompts(prev=> [...prev, input]);
     const response = await run(input);
-    setResultData(response);
-    setLoading(false);
+    let responseArray = response.split("**");
+    let newResponse;
+    for (let i =0; i < responseArray.length; i++) {
+      if(i=== 0 || i%2 !==1){
+        newResponse += responseArray[i];
+      }
+      else{
+        newResponse += "<b>" + responseArray[i] + "</b>";
+      }
+    }
+    let newResponse2 = newResponse.split("*").join("</br>");
+    let newResponseArray = newResponse2.split(" ");
+    for (let i =0; i < newResponseArray.length; i++) {
+      const nextword = newResponseArray[i];
+      delayPara(i, nextword+" "); 
+    }
+    setLoading(false); 
     setInput("");
   };
   
